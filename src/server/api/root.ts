4f3bb792542676908ac08f3bfeba6cecbba955ed
Rohit Sah <sahrohit9586@gmail.com>
2023-08-18 19:05:16 +0545

    hot fix

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { newsLetterRouter } from "./routers/newsletter";
 import { adminRouter } from "./routers/admin";
@@ -9,7 +8,6 @@ import { adminRouter } from "./routers/admin";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   newsletter: newsLetterRouter,
   admin: adminRouter,
 });
